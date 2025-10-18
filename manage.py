1046a927e3fc0626f679841cde57d0cce013ad39
Eniayeju <Eniayeju@JOSEY>
2025-10-18 17:02:01 +0100

    Add project files.

---
+++ manage.py
@@ -0,0 +1,12 @@
+﻿your_project/
+│
+├── manage.py
+├── your_app/
+│   ├── __init__.py
+│   ├── views.py
+│   ├── request.py   ← new module for the Cat Facts API
+│   └── urls.py
+└── your_project/
+    ├── __init__.py
+    ├── settings.py
+    ├── urls.py
